a83e7976d016471730e975f44628b50ad5bb7631
Demfiyke <ikennagochonma@gmail.com>
2023-06-17 03:27:20 -0700

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 	for (x = 48; x < 58; x++)
 	{
 		putchar(x);
-	}
-	if (x < 57)
-	{
-		putchar(44);
-		putchar(32);
+		if (x < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
 	putchar('\n');
 	return (0);
