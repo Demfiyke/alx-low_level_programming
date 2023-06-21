86176a603778e9c75bc357c9a072ffd933fb328d
Demfiyke <ikennagochonma@gmail.com>
2023-06-21 16:00:38 -0700

    correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,15 @@ int print_last_digit(int a)
 {
 	int b;
 
-	if (a < 0)
-	a = -a;
-
 	b = a % 10;
-
 	if (b < 0)
-		b = -a;
-
-	_putchar(b + '0');
-
-return (b);
+	{
+		_putchar(-b + 48);
+		return (-b);
+	}
+	else
+	{
+		_putchar(b + 48);
+		return (b);
+	}
 }
