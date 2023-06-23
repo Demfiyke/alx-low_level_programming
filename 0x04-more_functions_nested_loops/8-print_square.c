9a0afad1aed56a546ca5855d4283203aebbf1ee1
Demfiyke <ikennagochonma@gmail.com>
2023-06-23 00:09:00 -0700

    correction 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,18 +7,21 @@
  */
 void print_square(int size)
 {
-	int a, b;
-
-	for (a = 0; a < size; a++)
+	if (size <= 0)
 	{
-		for (b = 0; b < size; b++)
-		{
-			_putchar('#');
-		}
-		if (size <= 0)
-			_putchar('\n');
 		_putchar('\n');
+	} else
+	{
+		int a, b;
+
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
 
