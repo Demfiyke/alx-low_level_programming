fd388d035fd4019a3d1efa54501fb7a4ec123535
Demfiyke <ikennagochonma@gmail.com>
2023-07-01 12:17:03 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *z)
 	int x;
 
 	x = 0;
-	while (x < '\0')
+	while (x != '\0')
 	{
 		if (z[x] >= 'a' && z[x] <= 'z')
-			z[x] -= 32;
+			z[x] = z[x] - 32;
 		x++;
 	}
 	return (0);
